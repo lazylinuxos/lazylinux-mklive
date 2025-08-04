19a7ab9b9d3bd353c8849583198a5744f7bf1a4b
Karen Sargsyan <karyan40024@gmail.com>
2025-08-04 21:48:21 +0400

    Update theme, icons and fonts

--- mkiso.sh
+++ mkiso.sh
@@ -102,7 +102,7 @@ build_variant() {
             GFX_PKGS="xorg-video-drivers"
             GFX_WL_PKGS="mesa-dri"
             WANT_INSTALLER=yes
-            KERNEL_PKG="linux6.13"
+            KERNEL_PKG="linux6.15"
             TARGET_ARCH="$ARCH"
             ;;
         aarch64*)
