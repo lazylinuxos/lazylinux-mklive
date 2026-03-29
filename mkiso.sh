80c55c8944ddb825693581fcac8d4dde19ca60a9
Karen Sargsyan <karyan40024@gmail.com>
2026-03-29 21:19:02 +0400

    add workflow to build image

--- mkiso.sh
+++ mkiso.sh
@@ -102,7 +102,6 @@ build_variant() {
             GFX_PKGS="xorg-video-drivers xf86-video-intel"
             GFX_WL_PKGS="mesa-dri"
             WANT_INSTALLER=yes
-            KERNEL_PKG="linux6.19"
             TARGET_ARCH="$ARCH"
             ;;
         aarch64*)
